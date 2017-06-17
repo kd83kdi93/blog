69139df5e044473225938f4902686c2bed7418ec
bikli <460720965@qq.com>
2017-06-17 19:36:47 +0800

    增加了后端接口的权限检查功能

--- blog/src/main/java/interceptor/TouristInterceptor.java
+++ blog/src/main/java/interceptor/TouristInterceptor.java
@@ -11,31 +11,33 @@ import org.springframework.web.servlet.ModelAndView;
 import domain.User;
 import util.Result;
 
-public class ConsoleInterceptor implements HandlerInterceptor{
+public class TouristInterceptor implements HandlerInterceptor {
 
 	@Override
 	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
 			throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
 			throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2) throws Exception {
 		User user = (User) arg0.getSession().getAttribute("user");
-		if (user != null && user.getLevel()>1) {
+		if (user != null) {
 			return true;
 		}
 		Result result = new Result();
+		result.setData("权限不允许");
 		ObjectOutputStream out = new ObjectOutputStream(arg1.getOutputStream());
 		out.writeObject(result);
+		out.close();
 		return false;
 	}
 
